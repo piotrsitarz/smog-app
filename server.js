9949b2aa03edea4073fc8305c4eb90ae3ab8bc19
Piotr Sitarz <piotrsitarz@onet.eu>
2018-09-26 22:24:33 +0200

    dont parse undefined data from request

--- server.js
+++ server.js
@@ -32,48 +32,52 @@ app.get('/mainModel',(req, res) => {
       var iteration = 0;
       request(`https://api.gios.gov.pl/pjp-api/rest/station/sensors/${stationId}`, (err, res, body) => {
 
-        var sensors = JSON.parse(body);
-        sensorsCounter += sensors.length;
+        if (body !== undefined) {
+          var sensors = JSON.parse(body);
+          sensorsCounter += sensors.length;
 
-        for (let j = 0; j < sensors.length; j++) {
+          for (let j = 0; j < sensors.length; j++) {
 
-          request(`https://api.gios.gov.pl/pjp-api/rest/data/getData/${sensors[j].id}`, (err, res, body) => {
+            request(`https://api.gios.gov.pl/pjp-api/rest/data/getData/${sensors[j].id}`, (err, res, body) => {
 
-            var values = JSON.parse(body);
+              if (body !== undefined) {
+                var values = JSON.parse(body);
 
-            values = values.values.filter(function(param){
-               return param.value !== null
-            });
+                values = values.values.filter(function(param){
+                   return param.value !== null
+                });
 
-            values = values.splice(0,24);
-            values = values.reverse();
+                values = values.splice(0,24);
+                values = values.reverse();
 
-            for (let k = 0; k < values.length; k++) {
-              if (k === 0) {
-                values[k].date =('wczoraj ' + values[k].date.substr(values[k].date.indexOf(' ')+1).toString()).slice(0,-3);
-              } else if (k === 23) {
-                values[k].date =('dzisiaj ' + values[k].date.substr(values[k].date.indexOf(' ')+1).toString()).slice(0,-3);
-              } else {
-                values[k].date = (values[k].date.substr(values[k].date.indexOf(' ')+1).toString()).slice(0,-3);
+                for (let k = 0; k < values.length; k++) {
+                  if (k === 0) {
+                    values[k].date =('wczoraj ' + values[k].date.substr(values[k].date.indexOf(' ')+1).toString()).slice(0,-3);
+                  } else if (k === 23) {
+                    values[k].date =('dzisiaj ' + values[k].date.substr(values[k].date.indexOf(' ')+1).toString()).slice(0,-3);
+                  } else {
+                    values[k].date = (values[k].date.substr(values[k].date.indexOf(' ')+1).toString()).slice(0,-3);
+                  }
+                }
+
+                iteration++;
+
+                if (values.length >= 24) {
+
+                  modelData[i].sensors.push({
+                    sensorId:sensors[j].id,
+                    key:sensors[j].param.paramFormula,
+                    values:values
+                  });
+
+                }
+                if (sensorsCounter === iteration) {
+                   req.res.end(JSON.stringify(modelData));
+                }
               }
-            }
-
-            iteration++;
-
-            if (values.length >= 24) {
-
-              modelData[i].sensors.push({
-                sensorId:sensors[j].id,
-                key:sensors[j].param.paramFormula,
-                values:values
-              });
-
-            }
-            if (sensorsCounter === iteration) {
-               req.res.end(JSON.stringify(modelData));
-            }
-          });
-        };
+            });
+          };
+        }
       });
     };
   });
